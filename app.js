51289ddea7ad9d6f7ff12c8da14acc9bcca65893
Alan Ma <alan@katsudon.local>
2017-03-08 17:53:32 -0500

    added blinking cursor

--- app.js
+++ app.js
@@ -5,15 +5,17 @@
 		$scope.data = ['A', 'l', 'a', 'n', ' ', 'M', 'a', '.'];
 		$scope.transform_value = "translateX(0)";
 		$scope.show_intro = true;
-		(function doTimeout (i) {
-			setTimeout(function () {
-				if (i--) {
-					$scope.name += $scope.data[$scope.data.length-1-i];
-					$scope.$apply();
-					doTimeout(i);
-				}
-			}, 130);
-		})($scope.data.length);
+		setTimeout(function() {
+			(function doTimeout (i) {
+				setTimeout(function () {
+					if (i--) {
+						$scope.name += $scope.data[$scope.data.length-1-i];
+						$scope.$apply();
+						doTimeout(i);
+					}
+				}, 100);
+			})($scope.data.length);
+		}, 2000);
 		setTimeout(function() {
 			$scope.transform_value = "translateX(-120%)";
 			$scope.$apply();
@@ -21,7 +23,7 @@
 				$scope.show_intro = false;
 				$scope.$apply();
 			}, 400);
-		}, 1400);
+		}, 3400);
 	});
 })();
 
