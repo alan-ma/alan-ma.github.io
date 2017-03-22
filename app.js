48807329d849befc1bb559baccefb5748be611bc
Alan Ma <alan@katsudon.local>
2017-03-22 13:51:24 -0400

    css changes for consistency

--- app.js
+++ app.js
@@ -74,16 +74,26 @@
 
 		$scope.tabs = [
 			{
-				'name': 'About',
-				'value': 0
+				"name": "About",
+				"value": 0
 			},
 			{
-				'name': 'Work',
-				'value': 1
+				"name": "Work",
+				"value": 1
 			},
 			{
-				'name': 'Fun',
-				'value': 2
+				"name": "Fun",
+				"value": 2
+			}
+		];
+
+		$scope.projects = [
+			{
+				"name": "Hello World!",
+				"organization": "Noobs Inc.",
+				"tags": ["A", "B", "C"],
+				"tab": "work",
+				"description": "This is a cool project! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan vestibulum ullamcorper. Aliquam dolor eros, pellentesque vitae odio sed, lacinia semper purus. Sed fermentum, purus at convallis molestie, dui arcu dictum odio, in rutrum lorem urna sed augue. Morbi sollicitudin blandit odio non malesuada. Morbi id quam sed nisi malesuada pulvinar."
 			}
 		];
 
